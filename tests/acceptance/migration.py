7148ceaa0ec10b8782766670684e10d7a83963e5
Cleber Rosa <crosa@redhat.com>
2019-11-01 11:57:56 -0400

    Acceptance tests: prototype of migration test with defer
    This is a PoC of a migration test using '-incoming defer'.  There's
    one (or maybe more) open questions, related to how to make the
    destination VM transition into 'running' after the migration is
    considered completed on the source VM.
    
    Signed-off-by: Cleber Rosa <crosa@redhat.com>

--- tests/acceptance/migration.py
+++ tests/acceptance/migration.py
@@ -22,14 +22,14 @@ class Migration(Test):
 
     @staticmethod
     def migration_finished(vm):
-        return vm.command('query-migrate')['status'] in ('completed', 'failed')
+        migrate_info = vm.command('query-migrate')
+        return migrate_info.get('status') in ('completed', 'failed')
 
     def _get_free_port(self):
         port = network.find_free_port()
         if port is None:
             self.cancel('Failed to find a free port')
         return port
-
 
     def test_migration_with_tcp_localhost(self):
         source_vm = self.get_vm()
@@ -47,4 +47,29 @@ class Migration(Test):
         self.assertEqual(dest_vm.command('query-migrate')['status'], 'completed')
         self.assertEqual(source_vm.command('query-migrate')['status'], 'completed')
         self.assertEqual(dest_vm.command('query-status')['status'], 'running')
+        self.assertEqual(source_vm.command('query-status')['status'], 'postmigrate')
+
+    def test_migration_defer_multifd_on_localhost(self):
+        source_vm = self.get_vm()
+        dest_vm = self.get_vm('-incoming', 'defer')
+        dest_vm.launch()
+        dest_vm.qmp('migrate-set-capabilities',
+                    capabilities=[{'state': True, 'capability': 'multifd'}])
+        dest_uri = 'tcp:localhost:%u' % self._get_free_port()
+        dest_vm.qmp('migrate-incoming', uri=dest_uri)
+        source_vm.launch()
+        source_vm.qmp('migrate', uri=dest_uri)
+        self.assertEqual(source_vm.command('query-migrate')['status'], 'setup')
+
+        wait.wait_for(
+            self.migration_finished,
+            timeout=self.timeout,
+            step=0.1,
+            args=(source_vm, )
+        )
+
+        self.assertEqual(source_vm.command('query-migrate')['status'], 'completed')
+        self.assertEqual(dest_vm.command('query-status')['status'], 'inmigrate')
+        # TODO: how does/should the dest_vm transition to 'status' == 'running' ?
+        #self.assertEqual(dest_vm.command('query-status')['status'], 'running')
         self.assertEqual(source_vm.command('query-status')['status'], 'postmigrate')
